276129c28e99688f7ceb7cee12f09b4e00fe0d21
aravind <aravindlakshmanan007@gmail.com>
2020-01-30 15:11:47 +0530

    home page completed

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+// import { HTTP } from '@ionic-native/http/ngx';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
-  constructor() {}
-
+  urls : any;
+  constructor(private http: HttpClient) {
+    this.http.get('https://picsum.photos/v2/list').subscribe((response) => {
+      console.log(response);
+      this.urls = response;
+      // console.log(this.urls[2].url);
+    });
+  }
 }
